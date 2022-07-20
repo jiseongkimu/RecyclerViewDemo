940ab2118b3250e602ebc575f9789c8a27223041
jiseongkimu <jiseongkimu@gmail.com>
2022-07-20 10:46:39 +0900

    recyclerview with clicklistener

--- app/src/main/java/com/example/recyclerviewdemo/MainActivity.kt
+++ app/src/main/java/com/example/recyclerviewdemo/MainActivity.kt
@@ -3,14 +3,22 @@ package com.example.recyclerviewdemo
 import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
 
     // 아이템 리스트 정의
-    val fruitList = listOf<String>("mango", "apple", "banana", "guava", "lemon", "pear", "orange")
-
+    val fruitList = listOf<Fruit>(
+        Fruit("mango", "Joe"),
+        Fruit("apple", "frank"),
+        Fruit("banana", "Tom"),
+        Fruit("guava", "alex"),
+        Fruit("lemon", "joe"),
+        Fruit("pear", "aaa"),
+        Fruit("orange", "name")
+    )
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -23,6 +31,16 @@ class MainActivity : AppCompatActivity() {
         recyclerView.layoutManager = LinearLayoutManager(this)
 
         // 어댑터 설정, 상단에 정의한 아이템 리스트를 생성자에 매개변수로 전달해야함
-        recyclerView.adapter = MyRecyclerViewAdapter(fruitList)
+        recyclerView.adapter = MyRecyclerViewAdapter(fruitList) {
+                selectedItem: Fruit -> listItemClicked(selectedItem)
+        }
+    }
+
+    private fun listItemClicked(fruit: Fruit){
+        Toast.makeText(
+            this@MainActivity,
+            "Supplier Fruit is : ${fruit.supplier}",
+            Toast.LENGTH_LONG
+        ).show()
     }
 }
--- app/src/main/java/com/example/recyclerviewdemo/MyRecyclerViewAdapter.kt
+++ app/src/main/java/com/example/recyclerviewdemo/MyRecyclerViewAdapter.kt
@@ -3,11 +3,16 @@ package com.example.recyclerviewdemo
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ExpandableListView
 import android.widget.TextView
+import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
 
 // 메인으로부터 전달받을 매개변수를 생성자에 설정
-class MyRecyclerViewAdapter(val fruitList:List<String>) : RecyclerView.Adapter<MyViewHolder>() {
+class MyRecyclerViewAdapter(
+    private val fruitList:List<Fruit>,
+    private val clickListener:(Fruit)->Unit
+    ) : RecyclerView.Adapter<MyViewHolder>() {
 
 
     // 리스트 아이템(뷰홀더, 레이아웃)을 생성하는 곳, 템플릿(xml)이 필요함
@@ -19,13 +24,13 @@ class MyRecyclerViewAdapter(val fruitList:List<String>) : RecyclerView.Adapter<M
         // 뷰홀더 인스턴스를 리턴해야함. 정의는 MyViewHolder에서
         return MyViewHolder(listItem)
     }
-    //recyclerview가 viewholder를 가져와 데이터 연결할때 호출
-    //적절한 데이터를 가져와서 그 데이터를 사용하여 뷰홀더의 레이아웃에 채움(데이터 표시)
-    //holder : onCreateViewHolder에서 리턴한 뷰홀더
+    // recyclerview가 viewholder를 가져와 데이터 연결할때 호출
+    // 적절한 데이터를 가져와서 그 데이터를 사용하여 뷰홀더의 레이아웃에 채움(데이터 표시)
+    // holder : onCreateViewHolder에서 리턴한 뷰홀더
     override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
         // 뷰홀더 클래스에서 초기화한 뷰. 값 초기화 가능
         val fruit = fruitList[position]
-        holder.myTextView.text = fruit
+        holder.bind(fruit, clickListener)
     }
 
     // 상단에 정의한 아이템 리스트 크기를 대입
@@ -37,5 +42,13 @@ class MyRecyclerViewAdapter(val fruitList:List<String>) : RecyclerView.Adapter<M
 // RecyclerView.ViewHolder()는 뷰를 매개변수로 갖는다.
 // 뷰 컴포넌트를 가져와야 한다
 class MyViewHolder(val view: View): RecyclerView.ViewHolder(view){
-    val myTextView = view.findViewById<TextView>(R.id.tvName)
+    fun bind(fruit: Fruit, clickListener:(Fruit)->Unit){
+        val myTextView = view.findViewById<TextView>(R.id.tvName)
+        myTextView.text = fruit.name
+
+        view.setOnClickListener{
+          clickListener(fruit)
+        }
+    }
+
 }
